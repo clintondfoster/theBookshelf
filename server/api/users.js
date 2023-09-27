e18414987d5ecf7975c6af12426f8f115e34b77d
clinton foster <clintondavidfoster@gmail.com>
2023-09-27 15:24:38 -0400

    Updated

--- server/api/users.js
+++ server/api/users.js
@@ -29,7 +29,7 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", authenticateToken, async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
     try {
       const user = await prisma.users.findUnique({
         where: {
@@ -42,7 +42,7 @@ router.get("/:id", authenticateToken, async (req, res, next) => {
     }
   });
 
-  router.delete("/:id",  authenticateToken, async (req, res, next) => {
+  router.delete("/:id", async (req, res, next) => {
   
     try {
       const user = await prisma.users.delete({
@@ -56,91 +56,7 @@ router.get("/:id", authenticateToken, async (req, res, next) => {
     }
   });
   
-  //Register new User
-  router.post("/register", async (req, res, next) => {
-
-    const salt_rounds = 5;
-
-    try {
-      const { username, password } = req.body;
-
-      if(!username || !password) {
-        return res.status(400).send("Username and password are required");
-      }
-      const hashedPassword = await bcrypt.hash(password, salt_rounds);
-
-      const user = await prisma.users.create({
-        data: {
-          username,
-          password: hashedPassword }
-      });
-
-      const token = jwt.sign({ id: user.id}, process.env.JWT);
-
-      res.status(201).send({
-        token,
-        user: {
-          userId: user.id,
-          username: user.username
-        },
-      })
-    } catch (err) {
-      next(err);
-    }
-  });
   
-  //Login User
-  router.post("/login", async (req, res, next) => {
-
-    try {
-      console.log("login request recieved", req.body)
-      const { username, password } = req.body;
-      
-
-      const user = await prisma.users.findUnique({
-        where: { username },
-      });
-
-      console.log("User found", user)
-      if(!user) {
-        return res.status(401).send("Invalid Login");
-      }
-
-      const isValid = await bcrypt.compare(password, user.password);
-      console.log("password verification", isValid)
-
-      if(!isValid) {
-        return res.status(401).send("Invalid Login");
-    }
-
-    const token = jwt.sign({ id: user.id }, process.env.JWT)
-      res.send({
-        token,
-        user: {
-          userId: user.id,
-          username: user.username,
-        }
-      });
-    } catch (err) {
-      next(err);
-    }
-  });
-
-
-  router.get("/me", authenticateToken, async (req, res,next)=>{
-    if(!req.user){
-        return res.send({})
-    }
-    try{
-        const user = await prisma.users.findUnique({
-            where: {id: req.user.id},
-        })
-
-        res.send(user)
-    }catch(err){
-        next(err)
-    }
-});
 
   router.put("/:id", authenticateToken, async (req, res, next) => {
   
