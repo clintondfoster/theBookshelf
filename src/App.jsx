5f7b5f721395b4e43d77318d497a9696565f0abd
SerenaTPhan <pttnhann@gmail.com>
2023-10-02 14:15:42 -0500

    m

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import SingleBook from "./pages/SingleBook";
 import AuthForm from "./components/AuthForm"
 import {Route, Routes} from "react-router-dom";
-import Cart from "./pages/Cart";
 import UserProfile from "./components/UserProfile";
 import ViewCart from "./pages/ViewCart";
 
@@ -15,9 +14,7 @@ function App() {
     <Routes>
       <Route path="/home" element={<Home/>}/>
       <Route path="/search-results" element={<Search />} />
-      <Route path={"/book/:id"} element={<SingleBook/>}/>
-       {/* <Route path={"/cart"} element={<Cart/>}/> */}
-        
+      <Route path={"/book/:id"} element={<SingleBook/>}/>   
       <Route path={"/auth"} element={<AuthForm/>}/>
       <Route path={"/login"} element={<AuthForm/>}/>
       {/* <Route path={"/register"} element={<AuthForm/>}/> */}
--- src/pages/ViewCart.jsx
+++ src/pages/ViewCart.jsx
@@ -1,33 +1,33 @@
-
 import { useGetOrderProductQuery } from "../reducers/api";
 import { Link } from "react-router-dom";
 const ViewCart = () => {
+  const { data, isLoading } = useGetOrderProductQuery();
+  console.log(data);
 
-  const {data, isLoading } = useGetOrderProductQuery(); 
-  console.log(data)
- 
   return (
-    <div>         <h2>Your Cart</h2>
-    {isLoading ? (
-      <h1>Loading...</h1>
-    ) : data.cart.length === 0 ? (
-      <h1>No Books Found</h1>
-    ) : (
-      data.cart.map((i) => (
-        <div key={i.orderId}>
+    <div>
+      {" "}
+      <h2>Your Cart</h2>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : data.cart.length === 0 ? (
+        <h1>No Books Found</h1>
+      ) : (
+        data.cart.map((i) => (
+          <div key={i.orderId}>
             <h5>Order ID: {i.orderId}</h5>
             <h6>Book ID: {i.booksId}</h6>
             <h6>Quantity: {i.quantity}</h6>
-            <h6>Price: ${i.price}</h6>        
-        </div>
-      ))
-    )}
-   <Link to='/home' >Keep Shopping</Link>
-   <h4> Ready to Checkout?</h4>
-        <Link to="/">Checkout as Guest  </Link> <p>or</p>
-        <Link to="/auth">Sign in/Create an account </Link> 
-  </div>
-  
+            <h6>Price: ${i.price}</h6>
+          </div>
+        ))
+      )}
+     
+      <Link to="/home">Keep Shopping</Link>
+      <h4> Ready to Checkout?</h4>
+      <Link to="/">Checkout as Guest </Link> <p>or</p>
+      <Link to="/auth">Sign in/Create an account </Link>
+    </div>
   );
 };
 
