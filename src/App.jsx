c250c3fc6fd38283215768fb3699e0611b5951cb
SerenaTPhan <pttnhann@gmail.com>
2023-10-02 10:41:30 -0500

    cartPageLayout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import SingleBook from "./pages/SingleBook";
 import AuthForm from "./components/AuthForm"
 import {Route, Routes} from "react-router-dom";
+import Cart from "./pages/Cart";
 
 
 
@@ -10,10 +11,11 @@ function App() {
   return (
     <div className="App">
     <Routes>
-      <Route index element={<Home/>}/>
+      <Route path={"/home"} element={<Home/>}/>
       <Route path="/search-results" element={<Search />} />
       <Route path={"/book/:id"} element={<SingleBook/>}/>
       <Route path={"/auth"} element={<AuthForm/>}/>
+      <Route path={"/cart"} element={<Cart/>}/>
     </Routes>
     </div>
   );
