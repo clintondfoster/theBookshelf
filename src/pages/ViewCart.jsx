9619175ef64fe725c3b97aff101beec662e5de6a
Coreydonohue <cqdonohue@gmail.com>
2023-10-02 17:10:39 -0400

    cart functionality added

--- src/pages/ViewCart.jsx
+++ src/pages/ViewCart.jsx
@@ -1,32 +1,26 @@
 // import { useGetOpenOrderQuery } from "../reducers/orderproduct";
+import { useEffect, useState } from "react";
 import { useGetOrderProductQuery, useGetBookByIdQuery } from "../reducers/api";
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
 
 const ViewCart = () => {
 
-  const { data, isLoading } = useGetOrderProductQuery();
-  console.log(data);
+  useGetOrderProductQuery()
+  const cart = useSelector(state => state.cart)
+  console.log(cart);
 
-
-  // useEffect(() => {
-  //   window.location.reload();
-  // }, []);
 
   return (
     <div>
-      {isLoading ? (
-        <h1>Loading...</h1>
-      ) : data.cart.length === 0 ? (
-        <h1>No Books Found</h1>
-      ) : (
-        data.cart.map((i) => (
+      {cart.map((i) => (
           <div key={i.id}>
             <h2>Book ID: {i.booksId}</h2>
             <h2>Quantity: {i.quantity}</h2>
             <h2>Price: {i.price}</h2>
           </div>
         ))
-      )}
+      }
     </div>
   );
 };
--- store.js
+++ store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { storeApi } from "./src/reducers/api";
 import searchReducer from './src/reducers/searchSlice'
 import authReducer from "./src/reducers/authSlice"
+import cartReducer from "./src/reducers/api.js"
 // import { orderProductApi } from "./src/reducers/orderproduct";
 
 const store = configureStore({
@@ -9,6 +10,7 @@ const store = configureStore({
     [storeApi.reducerPath]: storeApi.reducer,
     search: searchReducer,
     auth: authReducer,
+    cart: cartReducer,
     // [orderProductApi.reducerPath]: orderProductApi.reducer,
 
   },
